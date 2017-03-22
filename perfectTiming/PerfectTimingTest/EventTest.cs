08df1e4ddb32f109cf6fd6bbcf1446a8ef37ab0f
Martin Krasňan <martin.krasnan@gmail.com>
2017-03-22 18:47:33 +0100

    Vytvoreny projekt

---
+++ perfectTiming/PerfectTimingTest/EventTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PerfectTimingTest
+{
+    [TestClass]
+    public class EventTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Projekt na predmet Extrémne programovanie
+## Časomiera
+
+drive:
+https://drive.google.com/drive/folders/0Bzg_vIF9VInranZBSlpsZWhMdUU?usp=sharing
