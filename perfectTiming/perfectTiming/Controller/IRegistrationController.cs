3c3f7297a375b86a7791c97cdff1ca04992f7643
Martin Krasňan <martin.krasnan@gmail.com>
2017-03-23 17:16:52 +0100

    added interfaces

---
+++ perfectTiming/perfectTiming/Controller/IRegistrationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using perfectTiming.Model;
+using perfectTiming.Helpers;
+
+
+namespace perfectTiming.Controller
+{
+    interface IRegistrationController
+    {
+        List<Registration> Registrations { get; set; }
+
+        RequestResult<Registration> Add(Registration item);
+        RequestResult<Registration> Update(Registration item);
+        RequestResult<Registration> Remove(Registration item);
+        RequestResult<Registration> RemoveRange(List<Registration> range);
+
+        RequestResult<List<Competitor>> GetCompetitors();
+        RequestResult<List<Category>> GetCategories();
+    }
+}
---
+++ perfectTiming/perfectTiming/Controller/RegistrationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using perfectTiming.Model;
+using perfectTiming.Helpers;
+
+namespace perfectTiming.Controller
+{
+    class RegistrationController : IRegistrationController
+    {
+        private List<Registration> _registrations;
+        public List<Registration> Registrations { get { return _registrations; } set { _registrations = value; } }
+        public List<Competitor> Competitors { get { return this.GetCompetitors().Data; } }
+        //public List<Competitor>
+
+        public RequestResult<Registration> Add(Registration item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestResult<List<Category>> GetCategories()
+        {
+            throw new NotImplementedException();
+            //return Registrations.Select(i => i.Category).ToList();
+        }
+
+        public RequestResult<List<Competitor>> GetCompetitors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestResult<Registration> Remove(Registration item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestResult<Registration> RemoveRange(List<Registration> range)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestResult<Registration> Update(Registration item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
