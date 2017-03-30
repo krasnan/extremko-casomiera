f04d32b25e7f36ffb2c3c9c688a5db381c4d3761
Dezko <thejozefcelko@gmail.com>
2017-03-30 14:25:48 +0200

    Testy pre Kategoriu

--- perfectTiming/PerfectTimingTest/UnitTests/CategoryTest.cs
+++ perfectTiming/PerfectTimingTest/UnitTests/CategoryTest.cs
@@ -12,23 +12,100 @@ namespace PerfectTimingTest.UnitTests
     public class CategoryTest
     {
         [TestMethod]
-        public void AddCategoryTest()
+        public void CategoryAdd()
         {
-            Assert.Fail();
+            CategoryController ctrl = new CategoryController();
+            Category c = new Category
+            {
+                name = "Women",
+                description = "Women Category"
+            };
+
+            Assert.AreEqual(Enums.RequestStatus.Success, ctrl.Add(c));
         }
 
         [TestMethod]
-        public void UpdateCategoryTest()
+        public void CategoryAddEmpty()
         {
-            Assert.Fail();
+            CategoryController ctrl = new CategoryController();
+            Category c = new Category
+            {
+                name = "",
+                description = "Women Category"
+            };
+            Assert.AreEqual(Enums.RequestStatus.Error, ctrl.Add(c));
+        }
+
+        public void CategoryAddSameName()
+        {
+            CategoryController ctrl = new CategoryController();
+            Category c = new Category
+            {
+                name = "EQname",
+                description = "EQdescription1"
+            };
+
+            Category c1 = new Category
+            {
+                name = "EQname",
+                description = "EQdescription1"
+            };
+
+            Assert.AreEqual(Enums.RequestStatus.Success, ctrl.Add(c));
+            Assert.AreEqual(Enums.RequestStatus.Error, ctrl.Add(c1));
+        }
+
+
+
+        [TestMethod]
+        public void CategoryUpdate()
+        {
+            CategoryController ctrl = new CategoryController();
+            Category old = new Category
+            {
+                name = "OldName",
+                description = "oldDescription"
+            };
+
+            ctrl.Add(old);
+            Category nnew = ctrl.Categories[ctrl.Categories.Count - 1];
+
+            // Change name
+            nnew.name = "NewName";
+            Assert.AreEqual(Enums.RequestStatus.Success, ctrl.Update(nnew).Status);
+            Assert.AreEqual(nnew.name, ctrl.Categories[ctrl.Categories.Count - 1].name);
+
+            // Name empty
+            old.name = "";
+            Assert.AreEqual(Enums.RequestStatus.Error, ctrl.Update(old).Status);
+            Assert.AreEqual(nnew.name, ctrl.Categories[ctrl.Categories.Count - 1].name);
         }
 
         [TestMethod]
-        public void RemoveCategoyTest()
+        public void CategoryUpdateSameName()
         {
-            Assert.Fail();
+            CategoryController ctrl = new CategoryController();
+
+            Category c1 = new Category { name = "SameName", description = "void" };
+            Category c2 = new Category { name = "SimilarName", description = "void" };
+
+            ctrl.Add(c1);
+            ctrl.Add(c2);
+
+            // Dve kategorie z rovnakym menom nemozu existovat
+            c2.name = "SameName";
+            Assert.AreEqual(Enums.RequestStatus.Error, ctrl.Update(c2).Status);
+
         }
 
+        [TestMethod]
+        public void CategoryRemoveSimple()
+        {
+            CategoryController ctrl = new CategoryController();
+            Category c = new Category { name = "Example", description = "void" };
 
+            ctrl.Add(c);
+            Assert.AreEqual(Enums.RequestStatus.Success, ctrl.Remove(c));
+        }
     }
 }
