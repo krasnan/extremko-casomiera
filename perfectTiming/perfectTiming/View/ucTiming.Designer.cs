191996113839f9a97356597bdd7f3872095bbdb1
Dezko <thejozefcelko@gmail.com>
2017-05-15 16:23:20 +0200

    Pridanie base funkcionality user controlera pre Casovanie udalosti

---
+++ perfectTiming/perfectTiming/View/ucTiming.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace perfectTiming.View
+{
+    partial class ucTiming
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTimer = new MetroFramework.Controls.MetroLabel();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroStyleExtender1 = new MetroFramework.Components.MetroStyleExtender(this.components);
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroStyleExtender2 = new MetroFramework.Components.MetroStyleExtender(this.components);
+            this.metroTile4 = new MetroFramework.Controls.MetroTile();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelResult = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelTimer.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.labelTimer.Location = new System.Drawing.Point(410, 45);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(105, 25);
+            this.labelTimer.TabIndex = 3;
+            this.labelTimer.Text = "00:00:00.000";
+
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(696, 298);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(124, 72);
+            this.metroTile1.TabIndex = 4;
+            this.metroTile1.Text = "Štart";
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.ActiveControl = null;
+            this.metroTile2.Location = new System.Drawing.Point(696, 220);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(124, 72);
+            this.metroTile2.TabIndex = 5;
+            this.metroTile2.Text = "Koniec";
+            this.metroTile2.UseSelectable = true;
+            this.metroTile2.Click += new System.EventHandler(this.metroTile2_Click);
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.ActiveControl = null;
+            this.metroTile3.Location = new System.Drawing.Point(696, 142);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(124, 72);
+            this.metroTile3.TabIndex = 6;
+            this.metroTile3.Text = "Pauza";
+            this.metroTile3.UseSelectable = true;
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.metroTextBox1.CustomButton.ForeColor = System.Drawing.Color.Coral;
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(139, 2);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(65, 65);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.metroTextBox1.FontWeight = MetroFramework.MetroTextBoxWeight.Bold;
+            this.metroTextBox1.Lines = new string[] {
+        "X"};
+            this.metroTextBox1.Location = new System.Drawing.Point(179, 112);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(207, 70);
+            this.metroTextBox1.TabIndex = 7;
+            this.metroTextBox1.Text = "X";
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.metroTextBox1_KeyPress);
+            // 
+            // metroTile4
+            // 
+            this.metroTile4.ActiveControl = null;
+            this.metroTile4.Location = new System.Drawing.Point(505, 235);
+            this.metroTile4.Name = "metroTile4";
+            this.metroTile4.Size = new System.Drawing.Size(97, 43);
+            this.metroTile4.TabIndex = 9;
+            this.metroTile4.Text = "Add";
+            this.metroTile4.UseSelectable = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(193, 259);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 0);
+            this.labelResult.TabIndex = 10;
+            // 
+            // ucTiming
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.metroTile4);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroTile3);
+            this.Controls.Add(this.metroTile2);
+            this.Controls.Add(this.metroTile1);
+            this.Controls.Add(this.labelTimer);
+            this.Name = "ucTiming";
+            this.Size = new System.Drawing.Size(842, 395);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroLabel labelTimer;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Components.MetroStyleExtender metroStyleExtender1;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Components.MetroStyleExtender metroStyleExtender2;
+        private MetroFramework.Controls.MetroTile metroTile4;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroLabel labelResult;
+    }
+}
---
+++ perfectTiming/perfectTiming/View/ucTiming.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using MetroFramework;
+
+namespace perfectTiming.View
+{
+    public partial class ucTiming : MetroFramework.Controls.MetroUserControl
+    {
+
+        Stopwatch stopwatch;
+        //Chybne zadanie cisla
+        bool incorrectInput = false;
+        double lastCorrectTime;
+        string lastCas;
+
+
+        public ucTiming()
+        {
+            InitializeComponent();
+            stopwatch = new Stopwatch();
+
+
+
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            // Pauza Button
+            stopwatch.Stop();
+            timer1.Stop();
+            metroTile1.Enabled = true;
+            metroTile2.Enabled = true;
+            metroTile3.Enabled = false;
+
+            metroTile1.Text = "Pokracuj";
+        }
+
+        // Start Button
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+
+
+
+            // Vycistenie policka pre zadavanie cisel
+            metroTextBox1.Text = "";
+            metroTextBox1.Focus();
+            stopwatch.Start();
+
+            // Prepnutie tlacidiel
+            metroTile1.Enabled = false;
+            metroTile2.Enabled = true;
+            metroTile3.Enabled = true;
+
+
+            // Zmenit statusu preteku
+            // TODO: Upravit v liste pretekov
+
+        }
+
+        // Koniec button
+        private void metroTile2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MetroMessageBox.Show(this,"Chcete naozaj ukočiť časovanie preteku ??", "Pozor !!!!", MessageBoxButtons.YesNo);
+            if (!(result == DialogResult.Yes))
+            {
+                return;
+            }
+
+            stopwatch.Stop();
+            timer1.Stop();
+            stopwatch = new Stopwatch();
+            labelTimer.Text= "00:00:00.000";
+
+            metroTile2.Enabled = false;
+            metroTile3.Enabled = false;
+
+
+
+            // Update stavu zavodu na ukonceny
+            // TODO: zmenti stav preteku
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = stopwatch.Elapsed;
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            labelTimer.Text = elapsedTime;
+
+        }
+
+        private void metroTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+
+
+                int cislo;
+
+                // kontrola ci bolo startovne cislo zadane do texboxu v spravnom tvare a to teda ako integer
+                if (int.TryParse(metroTextBox1.Text, out cislo) == false)
+                {
+
+                    labelResult.ForeColor = System.Drawing.Color.Red;
+                    labelResult.Text = "Chyba vstupu!! Vstupy môžu byť iba čísla !!";
+                    metroTextBox1.Text = "";
+                    metroTextBox1.Focus();
+                    return;
+                }
+
+
+                TimeSpan ts = stopwatch.Elapsed;
+                String cas = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+
+                double times = ts.TotalMilliseconds;
+
+                // Ak vsetko oki pridaj to do vysledkov
+
+            }
+        
+
+
+         
+
+        }
+
+    }
+}
